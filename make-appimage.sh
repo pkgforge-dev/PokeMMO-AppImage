f6945faa9092d41945b3f0f339de7158b2109040
Link4Electronics <link4electronics@gmail.com>
2026-03-15 12:22:18 -0300

    add java and openssl

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,24 +3,25 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q pokemmo | awk '{print $2; exit}') # example command to get version of application here
+VERSION=4.0
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/pokemmo-launcher.png
-export DESKTOP=/usr/share/applications/pokemmo.desktop
-export STARTUPWMCLASS=com.pokemmo.PokeMMO
 export DEPLOY_OPENGL=1
+export DEPLOY_PULSE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/pokemmo-launcher
+quick-sharun ./AppDir/bin/pokemmo-launcher \
+    /usr/lib/jvm/java* \
+    /usr/lib/libopenal.so* \
+    /usr/bin/openssl
 
-# Additional changes can be done in between here
+echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
