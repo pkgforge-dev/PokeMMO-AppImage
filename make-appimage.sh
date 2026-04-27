db314505e41e0a2d712c3d09991e5a91a4d0f3ed
Link4Electronics <link4electronics@gmail.com>
2026-04-27 10:50:42 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export STARTUPWMCLASS=com.pokemmo.PokeMMO
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-3.0
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
+export DEPLOY_PULSE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/pokemmo-launcher \
@@ -25,6 +25,7 @@ quick-sharun /usr/bin/pokemmo-launcher \
     /usr/bin/openssl
 
 # Additional changes can be done in between here
+echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
