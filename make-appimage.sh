f6ac509c8abd451bf08f50b02ea3f3bd08122ee7
Link4Electronics <link4electronics@gmail.com>
2026-08-29 21:04:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,8 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/pixmaps/pokemmo-launcher.png
 export DESKTOP=/usr/share/applications/pokemmo.desktop
 export STARTUPWMCLASS=com.pokemmo.PokeMMO
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_OPENGL=1
 export DEPLOY_PULSE=1
 
